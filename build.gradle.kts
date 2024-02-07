3520accd62c36a3d45abd22068f3fa44fc210474
ItzikTempleman <59420731+ItzikTempleman@users.noreply.github.com>
2024-02-07 09:25:08 +0200

    fixed build
    fixed build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
-
+val h2databas: String by project
 val exposed_version: String by project
 
 plugins {
@@ -35,7 +35,7 @@ dependencies {
     implementation("io.ktor:ktor-server-content-negotiation-jvm")
     implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
-    implementation("com.h2database:h2: 2.2.220")
+    implementation("com.h2database:h2: $h2databas")
     implementation("io.ktor:ktor-serialization-gson-jvm")
     implementation("io.ktor:ktor-server-default-headers-jvm")
     implementation("io.ktor:ktor-server-host-common-jvm")
