da6240fb224a2b6c3c6d9c3eb15008af9562a474
ItzikTempleman <59420731+ItzikTempleman@users.noreply.github.com>
2024-02-08 13:39:05 +0200

    d
    d

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
 }
-group = "com"
+group = "com.users"
 version = "0.0.1"
 
 application {
