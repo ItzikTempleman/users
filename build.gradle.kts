7279c4ff7d856c53c71ca1f170828c575fb6c504
ItzikTempleman <59420731+ItzikTempleman@users.noreply.github.com>
2024-02-08 06:57:28 +0200

    g
    b

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,8 +18,8 @@ group = "com.users"
 version = "0.0.1"
 
 application {
-    //mainClass.set("io.ktor.server.netty.EngineMain")
-    mainClass.set("com.usrs.Application.Kt")
+    mainClass.set("io.ktor.server.netty.EngineMain")
+    //mainClass.set("com.usrs.Application.Kt")
 
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
--- src/main/kotlin/com/users/Application.kt
+++ src/main/kotlin/com/users/Application.kt
@@ -3,9 +3,8 @@ package com.users
 import com.users.plugins.*
 import io.ktor.server.application.*
 
-fun main(args: Array<String>) {
-    io.ktor.server.netty.EngineMain.main(args)
-}
+fun main(args: Array<String>) = io.ktor.server.netty.EngineMain.main(args)
+
 
 fun Application.module() {
     configureSerialization()
